11b87ef66359e7ca3f70505d6c865491433a52e5
dev-b2gre <informatica@b2green.pt>
2026-04-17 16:19:08 +0100

    Substitui branding textual por logótipo no header

--- themes/accstage-custom/header.php
+++ themes/accstage-custom/header.php
@@ -15,7 +15,18 @@
 <?php wp_body_open(); ?>
 <header class="acc-site-header">
     <div class="acc-wrap">
-        <a class="acc-logo" href="<?php echo esc_url(home_url('/')); ?>">ACCSTAGE</a>
+        <?php
+        $acc_logo_relative = '/assets/images/Accstage_main_black.png';
+        $acc_logo_file     = get_template_directory() . $acc_logo_relative;
+        $acc_logo_url      = get_template_directory_uri() . $acc_logo_relative;
+        ?>
+        <a class="acc-logo" href="<?php echo esc_url(home_url('/')); ?>">
+            <?php if (file_exists($acc_logo_file)) : ?>
+                <img class="acc-logo__image" src="<?php echo esc_url($acc_logo_url); ?>" alt="<?php esc_attr_e('Accstage', 'accstage-custom'); ?>" />
+            <?php else : ?>
+                <span class="acc-logo__text"><?php esc_html_e('ACCSTAGE', 'accstage-custom'); ?></span>
+            <?php endif; ?>
+        </a>
 
         <nav class="acc-menu" aria-label="<?php esc_attr_e('Navegação principal', 'accstage-custom'); ?>">
             <a href="<?php echo esc_url(home_url('/projetos/')); ?>"><?php esc_html_e('Projetos', 'accstage-custom'); ?></a>
